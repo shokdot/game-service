32693840f2d58728a153c9ff7e7c7e897ba5bea8
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-11-07 20:21:19 +0400

    feat: conf init

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { FastifyInstance } from 'fastify';
+import { buildApp, startServer, API_PREFIX } from '@core/index.js';
+import { PORT, HOST, SERVICE_NAME } from './utils/env.js';
+import healthRoutes from '@core/routes/health.routes.js';
+import gameRoutes from 'src/routes/index.js';
+import ws from "@fastify/websocket";
+
+const app: FastifyInstance = buildApp(SERVICE_NAME);
+app.register(ws);
+
+async function registerRoutes(app: FastifyInstance) {
+	await app.register(healthRoutes, { prefix: `${API_PREFIX}/game` });
+	await app.register(gameRoutes, { prefix: `${API_PREFIX}/game` });
+}
+
+startServer(app, registerRoutes, HOST, PORT);
